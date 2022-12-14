b90f064615e2d4244d307fc82321601bbc5b6648
Shawn <shwnsmith12@gmail.com>
2022-12-14 10:07:44 -0600

    moved everything out of the Develop folder.

---
+++ models/Product.js
@@ -0,0 +1,59 @@
+// import important parts of sequelize library
+const { Model, DataTypes } = require('sequelize');
+// import our database connection from config.js
+const sequelize = require('../config/connection');
+
+// Initialize Product model (table) by extending off Sequelize's Model class
+class Product extends Model {}
+
+// set up fields and rules for Product model
+Product.init(
+  {
+      // Defining the ID Column
+      id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true
+      },
+      // Defining the Product_Name Column
+      product_name: {
+          type: DataTypes.STRING,
+          allowNull: false
+      },
+      // Defining Price Column
+      price: {
+          type: DataTypes.DECIMAL(10,2),
+          allowNull: false,
+          validate: {
+              isDecimal: true
+          }
+      },
+      // Defining Stock Column
+      stock: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          defaultValue: 10,
+          validate: {
+              isNumeric: true
+          }
+      },
+      // Defining Category_ID column
+      category_id: {
+          type: DataTypes.INTEGER,
+          references: {
+              model: "category",
+              key: "id"
+          }
+      }
+  },
+  {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'product',
+  }
+);
+
+module.exports = Product;
