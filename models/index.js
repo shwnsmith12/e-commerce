e30887d47f6fa5091a573e387047a74eaaf36834
Shawn <shwnsmith12@gmail.com>
2022-12-19 14:29:19 -0600

    fixed the belongsTo so it wasn't capitalized

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTO(Category, {
+Product.belongsTo(Category, {
   foreignKey: 'category_id'
 });
 // Categories have many Products
@@ -13,12 +13,12 @@ Category.hasMany(Product, {
   foreignKey: 'category_id'
 });
 // Products belongToMany Tags (through ProductTag)
-Product.belongsTOMany(Tag, {
+Product.belongsToMany(Tag, {
   through: ProductTag,
   foreignKey: 'product_id'
 });
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsTOMany(Products, {
+Tag.belongsToMany(Product, {
   through: ProductTag,
   foreignKey: 'tag_id'
 });
